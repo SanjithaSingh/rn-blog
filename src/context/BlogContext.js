3d3b119b4ee43f8f773f7b50c42a2e4370f2f429
Sanjitha Singh <sanjitha.singh@gmail.com>
2020-08-08 20:33:28 +0530

    added working context

---
+++ src/context/BlogContext.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import { set } from 'react-native-reanimated';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([]);
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }]);
+    }
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
+        {children}
+    </BlogContext.Provider>
+};
+
+export default BlogContext;
